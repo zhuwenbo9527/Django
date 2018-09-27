1514992af7aeca45b67042bfe762b57382b1971f
DESKTOP-2K89CIA\b2b <zwb@ivyb2b.com>
2018-09-27 21:04:28 +0800

    09-27

--- project/Django/guest/guest/urls.py
+++ project/Django/guest/guest/urls.py
@@ -21,5 +21,5 @@ urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^index/$', views.index),
     url(r'^login_action/$', views.login_action),
-    url(r'^login_action/$', views.login_action),
+    url(r'^event_manage/$', views.event_manage),
 ]
--- project/Django/guest/sign/views.py
+++ project/Django/guest/sign/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from django.contrib import auth
 
 # Create your views here.
 from django.http import HttpResponse,HttpResponseRedirect
@@ -9,8 +10,19 @@ def login_action(request):
     if request.method == 'POST':
         username = request.POST.get('username','')
         password = request.POST.get('password','')
-        if username == '123' and password =='123':
+        user = auth.authenticate(username=username,password=password)
+        if user is not None:
+            auth.login(request,user)
             #return HttpResponse('login success')
-            return HttpResponseRedirect('/event_manage')
+            #return HttpResponseRedirect('/event_manage/')
+            response=HttpResponseRedirect('/event_manage/')
+            #response.set_cookie('user',username,3600)#添加浏览器cookie
+            request.session['user']=username #将session信息记录到浏览器
+
+            return response
         else:
             return render(request,'index.html',{'error':'username or password error!'})
+
+def event_manage(request):
+    username=request.session.get('user','')
+    return render(request,"event_manage.html")
